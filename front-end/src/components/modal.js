ed35be1812a906837068fe0f5d75b15af5ce5a4c
arziev-a <alinurarziev@gmail.com>
2023-01-18 14:41:39 +0100

    -added some styling -implemented modal.js file in components folder -displayed data in modal component

--- front-end/src/components/modal.js
+++ front-end/src/components/modal.js
@@ -1,8 +1,44 @@
-import React from 'react'
-const Modal = ({ children, onClose }) => (
-  <div>
-    <div>{children}</div>
-    <button onClick={onClose}>Close</button>
-  </div>
-);
-export default Modal;
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal({handleProductSelect, selectedProduct, handleModalClose,modalOpen}) {
+  // const [open, setOpen] = React.useState(false);
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      {/* <Button onClick={handleProductSelect}>Open modal</Button> */}
+      <Modal
+        open={modalOpen}
+        onClose={handleModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+            <h1 style={{color: selectedProduct.color}}>{selectedProduct.name}</h1>
+            <p>ID: {selectedProduct.id}</p>
+            <p>Year: {selectedProduct.year}</p>
+            <p>Color: {selectedProduct.color}</p>
+            <p>Pantone value: {selectedProduct.pantone_value}</p>
+        </Box>
+        {/* <Button>Close</Button> */}
+      </Modal>
+    </div>
+  );
+}
